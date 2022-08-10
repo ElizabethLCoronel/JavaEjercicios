11871b16c6e8832032e23dac3ae61489c997893d
Lizzy <elizabethlpz@live.com.mx>
2022-08-09 20:05:17 -0500

    Ejercicios en Java

---
+++ src/com/OpenBootcamp/Ejercicio01.java
@@ -0,0 +1,35 @@
+package com.OpenBootcamp;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+
+        //1. numericos
+
+        //1.1 enteros
+        byte numEntero1 = 5;
+        short numEntero2 = 10;
+        int numEntero3 = 30;
+        long numEntero4 = 100;
+        System.out.println("Numeros enteros: \n" + numEntero1 + " , " + numEntero2 + " , " + numEntero3 + " , " + numEntero4);
+
+
+
+
+        //1.2 decimales
+        float numDecimal1 = 5.5f;
+        double numDecimal2 = 10.5d;
+        System.out.println("Decimales: \n" + numDecimal1 + " , " + numDecimal2);
+
+
+        //2. booleano
+        boolean falso = false;
+        boolean verdadero = true;
+
+
+        //3. texto
+        char unCaracter = 'a';
+        String cadenaTexto = "Lorem ipsum dolor sit amet...";
+
+        System.out.println("Textos: " + "\nTipo Char: "+ unCaracter + "" + "\nCadena de Texto: " +cadenaTexto );
+    }
+}
